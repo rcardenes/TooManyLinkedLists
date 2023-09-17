b4d72c91dbe7f2cbae10bb5a01d3ea0b0a2a0d6c
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-09-17 18:19:12 -0300

    Full implementation of the first example

--- src/first.rs
+++ src/first.rs
@@ -38,3 +38,44 @@ impl List {
         }
     }
 }
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Add extra elements
+        list.push(4);
+        list.push(5);
+
+        // Test normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Test exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
