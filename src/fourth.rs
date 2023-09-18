97fbfea679425737654be12d574edace55a17661
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-09-18 09:09:57 -0300

    Now we can peek from the front

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 
 pub struct List<T> {
@@ -62,6 +62,12 @@ impl<T> List<T> {
             // old_head.into_inner().elem
         })
     }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -101,5 +107,14 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
     }
 }
